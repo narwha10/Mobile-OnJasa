6dd233b9a27b53f59a7c902b70efaeaf3933dcdb
dp6adamali082 <adamnasution2018@gmail.com>
2024-10-24 11:41:09 +0700

    Chat on jasa

--- app/src/main/java/com/example/onjasa/Your_Activity.kt
+++ app/src/main/java/com/example/onjasa/Your_Activity.kt
@@ -1,18 +1,16 @@
 package com.example.onjasa
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class ChatActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
+class Your_Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_chat)
+        setContentView(R.layout.activity_your)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -20,5 +18,3 @@ class ChatActivity : AppCompatActivity() {
         }
     }
 }
-   // private fun (): Unit {
-    
